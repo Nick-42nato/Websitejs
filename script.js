55aa5b973311f4b516d400e1cfc7d2dfc4c2bc90
Nick-42nato <nfortunato007@gmail.com>
2017-10-16 13:41:45 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+function makeOpaque(e){
+	e.classList.toggle("opaque");
+}
 
+function makeOpaque1(e){
+	e.classList.toggle("opaque1");
+}
+
+function makeOpaque2(e){
+	e.classList.toggle("opaque2");
+}
+
+function makeOpaque3(e){
+	e.classList.toggle("opaque3");
+}
+
+function makeOpaque4(e){
+	e.classList.toggle("opaque4");
+}
