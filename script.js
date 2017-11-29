685883da05818363c17301e4f0579044dc498223
Nick-42nato <nfortunato007@gmail.com>
2017-11-29 14:13:04 -0800

    Update script.js

--- script.js
+++ script.js
@@ -23,32 +23,26 @@ function makeOpaque5(e){
 }
 
 function showText1(elem){
-	document.getElementById("box1").classList.toggle("tan");
 	elem.innerHTML = "Neon Tetras";
 }
 
 function showText2(elem){
-	document.getElementById("box2").classList.toggle("tan");
 	elem.innerHTML = "Golden tetras";
 }
 
 function showText3(elem){
-	document.getElementById("box3").classList.toggle("tan");
 	elem.innerHTML = "Diamond Blue Shrimp";
 }
 
 function showText4(elem){
-	document.getElementById("box4").classList.toggle("tan");
 	elem.innerHTML = "Mandarin Shrimp";
 }
 
 function showText5(elem){
-	document.getElementById("box5").classList.toggle("tan");
 	elem.innerHTML = "Cherry Shrimp";
 }
 
 function showText6(elem){
-	document.getElementById("box6").classList.toggle("tan");
 	elem.innerHTML = "Crystal red Shrimp";
 }
 
