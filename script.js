7c9e352bae62b89ce8299b874b56825400d42911
Nick-42nato <nfortunato007@gmail.com>
2017-11-29 13:19:02 -0800

    Update script.js

--- script.js
+++ script.js
@@ -22,6 +22,36 @@ function makeOpaque5(e){
 	e.classList.toggle("opaque5");
 }
 
+function showText1(elem){
+	document.getElementById("box1").classList.toggle("tan");
+	elem.innerHTML = "JavaScriptSorcery";
+}
+
+function showText2(elem){
+	document.getElementById("box1").classList.toggle("tan");
+	elem.innerHTML = "JavaScriptSorcery";
+}
+
+function showText3(elem){
+	document.getElementById("box1").classList.toggle("tan");
+	elem.innerHTML = "JavaScriptSorcery";
+}
+
+function showText4(elem){
+	document.getElementById("box1").classList.toggle("tan");
+	elem.innerHTML = "JavaScriptSorcery";
+}
+
+function showText5(elem){
+	document.getElementById("box1").classList.toggle("tan");
+	elem.innerHTML = "JavaScriptSorcery";
+}
+
+function showText6(elem){
+	document.getElementById("box1").classList.toggle("tan");
+	elem.innerHTML = "JavaScriptSorcery";
+}
+
 function makeHover(e){
 	e.classList.toggle("hover");
 }
