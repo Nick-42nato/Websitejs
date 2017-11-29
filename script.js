481bbf6241833eab692ff0e85045cd6a2d43ac23
Nick-42nato <nfortunato007@gmail.com>
2017-11-29 14:26:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,30 @@
 function makeOpaque(e){
 	e.classList.toggle("opaque");
+	elem.innerHTML = "Neon Tetras";
 }
 
 function makeOpaque1(e){
 	e.classList.toggle("opaque1");
+	elem.innerHTML = "Golden tetras";
 }
 
 function makeOpaque2(e){
 	e.classList.toggle("opaque2");
+	elem.innerHTML = "Diamond Blue Shrimp";
 }
 
 function makeOpaque3(e){
 	e.classList.toggle("opaque3");
+	elem.innerHTML = "Mandarin Shrimp";
 }
 
 function makeOpaque4(e){
 	e.classList.toggle("opaque4");
+	elem.innerHTML = "Cherry Shrimp";
 }
 
 function makeOpaque5(e){
 	e.classList.toggle("opaque5");
-}
-
-function showText1(elem){
-	elem.innerHTML = "Neon Tetras";
-}
-
-function showText2(elem){
-	elem.innerHTML = "Golden tetras";
-}
-
-function showText3(elem){
-	elem.innerHTML = "Diamond Blue Shrimp";
-}
-
-function showText4(elem){
-	elem.innerHTML = "Mandarin Shrimp";
-}
-
-function showText5(elem){
-	elem.innerHTML = "Cherry Shrimp";
-}
-
-function showText6(elem){
 	elem.innerHTML = "Crystal red Shrimp";
 }
 
